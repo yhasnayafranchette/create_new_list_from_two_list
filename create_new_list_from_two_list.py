aef01e8a7bc3bb5e2ca0f3ebe0e795a64c5e2fc8
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 23:04:12 +0800

    filtered new list

--- create_new_list_from_two_list.py
+++ create_new_list_from_two_list.py
@@ -1,7 +1,12 @@
-# Exercise 10: Create a new list from two list using the following condition
-# Create a new list from two list using the following condition
-# Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+#Exercise 10: Create a new list from two list using the following condition
+#Create a new list from two list using the following condition
+#Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
 
 #Create new list new that contain odd numbers from the first list and even numbers from the second list
+def create_new_list(list_one, list_two):
+    odd_numbers = [num for num in list_one if num % 2 != 0]
+    even_numbers = [num for num in list_two if num % 2 == 0]
+    new_list = odd_numbers + even_numbers
+    return new_list
 
 #Print new list
